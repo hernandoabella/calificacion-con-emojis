018bdbae1a1113df879a5ef76edef8b805f3a63b
Hernando Abella <hernandoabella@gmail.com>
2023-06-11 16:47:03 -0500

    Update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const starsEl = document.querySelectorAll(".fa-star");
 const emojisEl = document.querySelectorAll(".far");
 const colorsArray = ["#F87060", "#FFAD60", "#FFD35C", "#B7E267", "#54B1D0"];
 
-updateRating(0);
+updateRating(4);
 
 starsEl.forEach((starEl, index) => {
   starEl.addEventListener("click", () => {
