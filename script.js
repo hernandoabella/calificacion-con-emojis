29a981f1d2c3dfd73dae90a6ca38339e01bc35b4
Hernando Abella <hernandoabella@gmail.com>
2023-06-15 01:07:10 -0500

    Update

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-const starsEl = document.querySelectorAll(".fa-star");
-const emojisEl = document.querySelectorAll(".far");
-const colorsArray = ["#F87060", "#FFAD60", "#FFD35C", "#B7E267", "#54B1D0"];
+// Esperar a que el contenido HTML se haya cargado completamente
+document.addEventListener("DOMContentLoaded", () => {
+  // Obtener elementos del DOM
+  const starsEl = document.querySelectorAll(".fa-star"); // Elementos de estrellas
+  const emojisEl = document.querySelectorAll(".emoji"); // Elementos de emojis
 
-updateRating(4);
+  // Obtener la puntuación guardada en el almacenamiento local o establecer 5 estrellas como valor predeterminado
+  let rating = localStorage.getItem("rating") || 5;
 
-starsEl.forEach((starEl, index) => {
-  starEl.addEventListener("click", () => {
-    updateRating(index);
+  // Actualizar la visualización inicial de la puntuación
+  updateRating(rating);
+
+  // Asignar eventos de clic a las estrellas
+  starsEl.forEach((starEl, index) => {
+    starEl.addEventListener("click", () => {
+      updateRating(index + 1);
+    });
   });
+
+  // Función para actualizar la puntuación y los emojis
+  function updateRating(index) {
+    // Actualizar la visualización de las estrellas
+    starsEl.forEach((starEl, idx) => {
+      if (idx < index) {
+        starEl.classList.add("active");
+      } else {
+        starEl.classList.remove("active");
+      }
+    });
+
+    // Ocultar todos los emojis
+    emojisEl.forEach((emojiEl) => {
+      emojiEl.style.display = "none";
+    });
+
+    // Mostrar el emoji correspondiente a la puntuación seleccionada
+    emojisEl[index - 1].style.display = "block";
+
+    // Guardar la puntuación en el almacenamiento local
+    localStorage.setItem("rating", index);
+  }
 });
-
-function updateRating(index) {
-  starsEl.forEach((starEl, idx) => {
-    if (idx < index + 1) {
-      starEl.classList.add("active");
-    } else {
-      starEl.classList.remove("active");
-    }
-  });
-
-  emojisEl.forEach((emojiEl) => {
-    emojiEl.style.transform = `translateX(-${index * 50}px)`;
-    emojiEl.style.color = colorsArray[index];
-  });
-}
